20908b89f6e3a2c7d163dff58295740f1dd72389
Moelf <jerryling315@gmail.com>
2018-11-19 15:40:01 -0800

    Add pip uproot install

--- src/UpROOT.jl
+++ src/UpROOT.jl
@@ -1,17 +1,17 @@
 __precompile__()
 module UpROOT
-
 export getPyEle
 
 using PyCall, Gadfly
 #This is to make sure python pointer don't break things
 const uproot= PyNULL()
 function __init__()
-    copy!(uproot, pyimport_conda("uproot", "uproot"))
+    copy!(uproot, pyimport("uproot"))
 end
 
 struct TFile
     path::String
+    pyObj::PyObject
     #= procname::String =#
     keys::Array
 end
@@ -24,20 +24,24 @@ end
 getPyEle(pyDict, member::String) = PyCall.get(pyDict, PyCall.PyObject, member)
 
 function readTFile(path::String)
-    file = uproot[:open](path)
-    keys = file[:allkeys]()
-    println("List of keys: in $path")
-    println(keys)
-    return TFile(path, keys)
+    pyobj = uproot[:open](path)
+    keys = pyobj[:allkeys]()
+    #= println("List of keys: in $path") =#
+    #= println(keys) =#
+    return TFile(path, pyobj, keys)
 end
 
 function getHist(x::TFile, histName::String)
-    rfile = uproot[:open](x.path)
-    hist = getPyEle(rfile, histName)
+    hist = getPyEle(TFile.pyobj, histName)
     return TH1D(hist[:numpy][1],hist[:numpy][2][1:end-1])
 end
 
 function plotHist(hist::TH1D, procname::String)
+    Gadfly.plot(x=hist.Bins, y = hist.Counts, Geom.bar(position=:dodge),Guide.xlabel(procname))
+end
+
+function plotHist(tfile::TFile, histName::String, procname::String)
+    hist = getHist(tfile, histName)
     Gadfly.plot(x=hist.Bins, y = hist.Counts, Geom.bar(position=:dodge),Guide.xlabel(procname))
 end
 
